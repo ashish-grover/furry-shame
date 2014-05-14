313353e0715cc04f538d37fb841f5977307531ef
Ashish Grover <grovera@gmail.com>
2014-05-14 10:01:12 -0400

    updated index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>ThickMoney</title>
+		<title>Paisa Vasool</title>
 		<link rel="stylesheet" href="main.css">
 		
 		<link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
@@ -16,7 +16,7 @@
 			  <li><a href="#">Browse</a></li>
 			</ul>
 			<ul class="pull-right">
-			  <li><a href="#">Sign Up</a></li>
+			  <li><a href="register.php">Sign Up</a></li>
 			  <li><a href="#">Log In</a></li>
 			  <li><a href="#">Help</a></li>
 			</ul>
@@ -25,10 +25,9 @@
 	
 		<div class="jumbo">
 			<div class="container">
-				<h1>Welcome to ThickMoney!</h1>
-				<p>You will never want to leave because you will save a lot of money by staying!</p>	
+				<h1>Welcome to Paisa Vasool!</h1>
+				<p>Saving money has never been easier.</p>	
 			</div>			
 		</div>
-		
 	</body>
 </html>
